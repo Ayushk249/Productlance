2fc9592b1d8af942024bea3173cd97853a50da4d
Ayush Kumar Das <daskumarayush@gmail.com>
2023-06-15 17:23:30 +0530

    API requests, nodemon and concurrently as dev dependencies

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products= [
+{
+    _id : '1',
+    name: 'Airpods wireless Bluetooth headphones',
+    image: '/images/airpods.jpg',
+    description:
+    'dhjjdvhjdbjhdbjdbhdbhbdsbdbd high quality device lets you connect with compatible devices',
+    brand : 'Apple',
+    category: 'Electronics',
+    price: '10,000',
+    countInStock: 10,
+    rating :'4.5',
+    numReviews:'12'
+},
+{
+    _id : '2',
+    name: 'iPhone 11 Pro 256 GB memory',
+    image: '/images/phone.jpg',
+    description:
+    'dhjjdvhjdbjhdbjdbhdbhbdsbdbd high quality device lets you connect with compatible devices',
+    brand : 'Apple',
+    category: 'Electronics',
+    price: '48,000',
+    countInStock: 7,
+    rating :'5.2',
+    numReviews:'10'
+},
+{
+    _id : '3',
+    name: 'Canon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+    'dhjjdvhjdbjhdbjdbhdbhbdsbdbd high quality device lets you connect with compatible devices',
+    brand : 'Canon',
+    category: 'Electronics',
+    price: '72,000',
+    countInStock: 5,
+    rating :'3',
+    numReviews:'12'
+},
+{
+    _id : '4',
+    name: 'Sony Playstation',
+    image: '/images/playstation.jpg',
+    description:
+    'dhjjdvhjdbjhdbjdbhdbhbdsbdbd high quality device lets you connect with compatible devices',
+    brand : 'Sony',
+    category: 'Electronics',
+    price: '32,999',
+    countInStock: 7,
+    rating :'5',
+    numReviews:'15'
+},
+{
+    _id : '5',
+    name: 'Logitech G-series Gaming mouse',
+    image: '/images/mouse.jpg',
+    description:
+    'dhjjdvhjdbjhdbjdbhdbhbdsbdbd high quality device lets you connect with compatible devices',
+    brand : 'Logitech',
+    category: 'Electronics',
+    price: '3,999',
+    countInStock: 8,
+    rating :'3.8',
+    numReviews:'8'
+},
+{
+    _id : '6',
+    name: 'Amazon Echodot 3rd Generation ',
+    image: '/images/alexa.jpg',
+    description:
+    'dhjjdvhjdbjhdbjdbhdbhbdsbdbd high quality device lets you connect with compatible devices',
+    brand : 'Amazon',
+    category: 'Electronics',
+    price: '2,499',
+    countInStock: 0,
+    rating :'4',
+    numReviews:'12'
+}
+]
+
+export default products
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+import express from 'express'
+import products from './data/products.js'
+// frontend running on 3000
+const port = 5000
+
+const app= express()
+
+app.get('/' , (request,response) => {
+    response.send("API is running....")
+})
+
+app.get('/api/products', (request,response) => {
+    response.json(products)
+})
+
+app.get('/api/products/:id', (request,response) => {
+    response.json(products.find((product) => (product._id === request.params.id)))
+})
+
+app.listen(port,()=> console.log(`server running on ${port}`))
+
